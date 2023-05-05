96967695ca4d007ae972f54600f4050603d71a9c
Nishant5789 <nishantbhandari218@gmail.com>
2023-05-05 22:42:23 +0530

    first commit

---
+++ functions/api.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors'); 
+const serverless = require('serverless-http');
+const app = express();
+
+app.use(cors());
+const port = process.env.PORT || 3005;
+const apidata = require('../data/match.js');
+
+app.get('/', (req, res) => {
+    res.send(apidata);
+});
+
+app.listen(port, (req, res) => {
+    console.log("server started");
+});
+
+app.use('/.netlify/funtions/api', express.Router)
+module.exports.handler = serverless(app);
